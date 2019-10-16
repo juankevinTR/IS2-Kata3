be9e6bc34fd1a2bf4faca6de90842fc7bed322da
Juan Kevin Trujillo <juankevin.tr@gmail.com>
2019-10-16 10:21:49 +0100

    Version 1 and 2 of this kata

--- src/com/juankevintrujillo/Main.java
+++ src/com/juankevintrujillo/Main.java
@@ -8,6 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
         
+        Histogram<String> histogram = new Histogram();
+
+        histogram.increment("gmail.com");
+        histogram.increment("yahoo.com");
+        histogram.increment("outlook.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+
+        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        histogramDisplay.execute();
+        
     }
     
 }
